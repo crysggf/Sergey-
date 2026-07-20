8190c76806915fb8a126ce87676992167d98aa9c
Сергей <.>
2026-07-20 19:32:23 +0400

    Исправленное домашнее задание №3

--- Lesson_3_task_3.py/address.py
+++ Lesson_3_task_3.py/address.py
@@ -1,5 +1,5 @@
 class Address:
-    def __init__ (self, index, city, street, house, apartment):
+    def __init__(self, index, city, street, house, apartment):
         self.index = index
         self.city = city
         self.street = street
@@ -7,4 +7,4 @@ class Address:
         self.apartment = apartment
 
     def __str__(self):
-        return f"{self.index}, {self.city}, {self.street}, {self.house} - {self.apartment}"    
+        return f"{self.index}, {self.city}, {self.street}, {self.house} - {self.apartment}"
